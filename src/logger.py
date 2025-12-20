7899eba405c2230962153e8ee07930e80ccf8139
milii048 <milind.brt@gmail.com>
2025-12-20 21:16:17 +0530

    added data

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('f%m_%d_%Y_%H_%M_%S')}.log"
 logs_dir=os.path.join(os.getcwd(),"logs")
-os.makedirs(logs_dir)
+os.makedirs(logs_dir, exist_ok=True)
 LOG_FILE_PATH=os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(
